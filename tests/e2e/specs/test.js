598be27b707a69ddec8e3831c7ad191875c0e951
Wen Sun <syl1887415157@126.com>
2019-08-18 14:06:40 +0800

    chore: e2e test conf

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,7 +1,19 @@
 // For authoring Nightwatch tests, see
 // http://nightwatchjs.org/guide#usage
+// const chromedriver = require('chromedriver');
 
 module.exports = {
+  // selenium: {
+  //   cli_args: {
+  //     'webdriver.chrome.driver': chromedriver.path
+  //   }
+  // },
+  desiredCapabilities: {
+    browserName: "chrome",
+    chromeOptions: {
+      w3c: false
+    }
+  },
   'default e2e tests': browser => {
     browser
       .url(process.env.VUE_DEV_SERVER_URL)
@@ -9,6 +21,6 @@ module.exports = {
       .assert.elementPresent('.hello')
       .assert.containsText('h1', 'Welcome to Your Vue.js + TypeScript App')
       .assert.elementCount('img', 1)
-      .end()
+      .end();
   }
-}
+};
