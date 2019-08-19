630062a89570fc4053efde27c72987c06e334a94
Wen Sun <syl1887415157@126.com>
2019-08-19 19:43:48 +0800

    fix: update travis & nightwatch conf

--- nightwatch.config.js
+++ nightwatch.config.js
@@ -9,6 +9,7 @@ module.exports = {
         javascriptEnabled: true,
         acceptSslCerts: true,
         chromeOptions: {
+          args: ['--headless', '--no-sandbox', '--disable-gpu'],
           w3c: false
         }
       }
